d652c6d601ec73a2014e9c81fdf942c634658b9d
jesonzhong <1447433745@qq.com>
2017-11-16 18:24:38 +0800

    1.去除一些敏感的图片信息，涉及到暗黑战神的版权问题。

--- Server/Hotfix/System/NetOuterComponentSystem.cs
+++ Server/Hotfix/System/NetOuterComponentSystem.cs
@@ -25,14 +25,14 @@ namespace Hotfix
 	{
 		public static void Awake(this NetOuterComponent self)
 		{
-			self.Awake(NetworkProtocol.TCP);
+			self.Awake(NetworkProtocol.UDP);
             self.MessagePacker = new MongoPacker();
 			self.MessageDispatcher = new OuterMessageDispatcher();
 		}
 
 		public static void Awake(this NetOuterComponent self, string host, int port)
 		{
-			self.Awake(NetworkProtocol.TCP, host, port);
+			self.Awake(NetworkProtocol.UDP, host, port);
 			self.MessagePacker = new MongoPacker();
             self.MessageDispatcher = new OuterMessageDispatcher();
 		}
